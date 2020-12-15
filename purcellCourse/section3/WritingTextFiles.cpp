7b749a0a3ac0bdeec62a9717b214b3130889eb39
lateNightJava <martinez.m31415@gmail.com>
2020-12-15 13:28:36 -0500

    Using fstream to write to files

---
+++ purcellCourse/section3/WritingTextFiles.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::string fileName = "text.txt";
+
+    // // first way of doing this
+    // std::ofstream file;
+    // file.open(fileName);
+
+    // second way
+    std::fstream file;
+    file.open(fileName, std::ios::out);
+
+    if (file.is_open()) {
+        file << "Testing, testing" << std::endl;
+        file << 123 << std::endl;
+
+        for (int i = 1; i <= 10; i++) {
+            file << i << ". testing" << std::endl;
+        }
+    } else {
+        std::cout << "Unable to open: " << fileName << std::endl;
+    }
+    return 0;
+};
+
